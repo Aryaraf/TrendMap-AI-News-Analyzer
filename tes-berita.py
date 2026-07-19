a87807bdcdac4fcb3b655ee8ff1355a9497d1232
Aryaraf <aryaraf711@gmail.com>
2026-07-19 18:03:43 +0700

    feat: add .env support and enhance news analysis functionality

--- tes-berita.py
+++ tes-berita.py
@@ -1,14 +1,43 @@
 import requests
+import os
+from google.genai import Client 
+from dotenv import load_dotenv
 
-NEWS_API_KEY = "ca12fd888efe42ceacdb3db9d4231f75" 
+load_dotenv()
+
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
 topik = "technology asia"
-
 url = f"https://newsapi.org/v2/everything?q={topik}&apiKey={NEWS_API_KEY}&language=en&pageSize=3"
 response = requests.get(url)
 data = response.json()
 
-print("--- 3 NEWS UPDATE ---")
-for artikel in data['articles']:
-    print(f"Judul: {artikel['title']}")
-    print(f"Sumber: {artikel['source']['name']}")
-    print("-" * 20)
+kumpulan_berita = ""
+for i, artikel in enumerate(data['articles']):
+    kumpulan_berita += f"{i+1}. Judul: {artikel['title']}\n"
+
+print("--- BERITA YANG DIDAPAT ---")
+print(kumpulan_berita)
+
+print("Sedang menganalisis sentimen menggunakan Gemini... ⏳\n")
+
+prompt = f"""
+Kamu adalah seorang analis data ahli. Tolong baca daftar berita berikut:
+
+{kumpulan_berita}
+
+Tugasmu:
+1. Berikan kesimpulan sentimen keseluruhan (POSITIF / NEGATIF / NETRAL) dari berita-berita di atas.
+2. Berikan alasan singkat dalam 1-2 kalimat kenapa kamu memberikan penilaian tersebut.
+"""
+
+client = Client(api_key=GEMINI_API_KEY)
+
+hasil = client.models.generate_content(
+    model='gemini-3.5-flash',
+    contents=prompt
+)
+
+print("--- HASIL ANALISIS AI ---")
+print(hasil.text)
